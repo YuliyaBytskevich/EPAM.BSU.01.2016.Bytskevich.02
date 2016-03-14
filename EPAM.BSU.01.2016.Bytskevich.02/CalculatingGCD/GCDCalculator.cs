dc35364e1d5851ef33e508ed8fc48bc6447f318f
Yuliya Bytskevich <yuliya.bytskevich@gmail.com>
2016-03-14 11:24:12 +0300

    GCD Calculator class: checks added, unit tests (VS Unit Testing Framework) added

--- EPAM.BSU.01.2016.Bytskevich.02/CalculatingGCD/GCDCalculator.cs
+++ EPAM.BSU.01.2016.Bytskevich.02/CalculatingGCD/GCDCalculator.cs
@@ -11,75 +11,99 @@ namespace CalculatingGCD
     {
         public static int GetGCD(int firstNumber, int secondNumber, out string elapsedTime)
         {
-            int result;
-            Stopwatch watch = new Stopwatch();
-            watch.Start();
-            result = GetGCDOfTwoNumbers(firstNumber, secondNumber);
-            watch.Stop();
-            TimeSpan ts = watch.Elapsed;
-            elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds);
-            return result;
+            return RunCalculationWithChecks(firstNumber, secondNumber, out elapsedTime, GetGCDOfTwoNumbers);
         }
 
         public static int GetGCD(int firstNumber, int secondNumber, int thirdNumber, out string elapsedTime)
         {
-            Stopwatch watch = new Stopwatch();
-            watch.Start();
-            int intermediateGCD = GetGCDOfTwoNumbers(firstNumber, secondNumber);
-            int result = GetGCDOfTwoNumbers(intermediateGCD, thirdNumber);
-            watch.Stop();
-            TimeSpan ts = watch.Elapsed;
-            elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds);
-            return result;
+            return RunCalculationWithChecks(firstNumber, secondNumber, thirdNumber, out elapsedTime, GetGCDOfTwoNumbers);
         }
 
         public static int GetGCD(out string elapsedTime, params int[] numbers)
         {
-            Stopwatch watch = new Stopwatch();
-            watch.Start();
-            int intermediateGCD = numbers[0];
-            for (int i = 1; i< numbers.Length; i++)
-            {
-                intermediateGCD = GetGCDOfTwoNumbers(intermediateGCD, numbers[i]);
-            }
-            watch.Stop();
-            TimeSpan ts = watch.Elapsed;
-            elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds);
-            return intermediateGCD;
+            return RunCalculationWithChecks(out elapsedTime, numbers, GetGCDOfTwoNumbers);
         }
 
         public static int GetGCDWithBinaryAlgorithm(int firstNumber, int secondNumber, out string elapsedTime)
         {
-            int result;
-            Stopwatch watch = new Stopwatch();
-            watch.Start();
-            result = GetBinaryGCDOfTwoNumbers(firstNumber, secondNumber);
-            watch.Stop();
-            TimeSpan ts = watch.Elapsed;
-            elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds);
-            return result;
+            return RunCalculationWithChecks(firstNumber, secondNumber, out elapsedTime, GetBinaryGCDOfTwoNumbers);
         }
 
         public static int GetGCDWithBinaryAlgorithm(int firstNumber, int secondNumber, int thirdNumber, out string elapsedTime)
         {
+            return RunCalculationWithChecks(firstNumber, secondNumber, thirdNumber, out elapsedTime, GetBinaryGCDOfTwoNumbers);
+        }
+
+        public static int GetGCDWithBinaryAlgorithm(out string elapsedTime, params int[] numbers)
+        {
+            return RunCalculationWithChecks(out elapsedTime, numbers, GetBinaryGCDOfTwoNumbers);
+        }
+
+        private static int RunCalculationWithChecks(int firstNumber, int secondNumber, out string elapsedTime, Func<int, int, int> algorithm)
+        {
+            int result;
             Stopwatch watch = new Stopwatch();
             watch.Start();
-            int intermediateGCD = GetBinaryGCDOfTwoNumbers(firstNumber, secondNumber);
-            int result = GetBinaryGCDOfTwoNumbers(intermediateGCD, thirdNumber);
+            result = algorithm(firstNumber, secondNumber);
             watch.Stop();
             TimeSpan ts = watch.Elapsed;
             elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds);
             return result;
         }
 
-        public static int GetGCDWithBinaryAlgorithm(out string elapsedTime, params int[] numbers)
+        private static int RunCalculationWithChecks(int firstNumber, int secondNumber, int thirdNumber, out string elapsedTime, Func<int, int, int> algorithm)
         {
+            int result;
             Stopwatch watch = new Stopwatch();
             watch.Start();
-            int intermediateGCD = numbers[0];
-            for (int i = 1; i < numbers.Length; i++)
+            if (!(firstNumber == 0 && secondNumber == 0 && thirdNumber == 0))
             {
-                intermediateGCD = GetBinaryGCDOfTwoNumbers(intermediateGCD, numbers[i]);
+                if (!(firstNumber == 0 && secondNumber == 0))
+                {
+                    int intermediateGCD = algorithm(firstNumber, secondNumber);
+                    result = algorithm(intermediateGCD, thirdNumber);
+                }
+                else
+                {
+                    result = Math.Abs(thirdNumber);
+                }
+            }
+            else
+                throw new ArgumentException("At least one argument must be not null.");
+            watch.Stop();
+            TimeSpan ts = watch.Elapsed;
+            elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds);
+            return result;
+        }
+
+        private static int RunCalculationWithChecks(out string elapsedTime,  int[] numbers, Func<int, int, int> algorithm)
+        {
+            elapsedTime = "00:00:00:00";
+            int numOfNullArguments = 0;
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            if (numbers.Length < 2)
+                throw new ArgumentException("It is necessary to give a method at least two numbers.");
+            foreach (int num in numbers)
+                if (num == 0)
+                    numOfNullArguments++;
+            if (numOfNullArguments == numbers.Length)
+                throw new ArgumentException("At least one argument must be not null.");
+
+            if (numOfNullArguments == numbers.Length - 1)
+                return Math.Abs(numbers.Sum());
+            int i = 1;
+            int intermediateGCD;
+            if (numbers[0] == 0 && numbers[1] == 0)
+            {
+                intermediateGCD = numbers[1];
+                i = 2;
+            }
+            else
+                intermediateGCD = numbers[0];
+            for (; i < numbers.Length; i++)
+            {
+                intermediateGCD = algorithm(intermediateGCD, numbers[i]);
             }
             watch.Stop();
             TimeSpan ts = watch.Elapsed;
@@ -89,6 +113,16 @@ namespace CalculatingGCD
 
         private static int GetGCDOfTwoNumbers(int firstNumber, int secondNumber)
         {
+            if (firstNumber == 0 && secondNumber == 0)
+                throw new ArgumentException("At least one argument must be not null.");
+            if (firstNumber == 0 )
+                return Math.Abs(secondNumber);
+            if (secondNumber == 0)
+                return Math.Abs(firstNumber);
+            if (firstNumber < 0)
+                firstNumber = Math.Abs(firstNumber);
+            if (secondNumber < 0)
+                secondNumber = Math.Abs(secondNumber);
             while (firstNumber != secondNumber)
             {
                 if (firstNumber > secondNumber)
@@ -101,34 +135,29 @@ namespace CalculatingGCD
 
         private static int GetBinaryGCDOfTwoNumbers(int firstNumber, int secondNumber)
         {
-            int shift;
+            if (firstNumber == 0 && secondNumber == 0)
+                throw new ArgumentException("At least one argument must be not null.");
             if (firstNumber == 0)
                 return secondNumber;
             else if (secondNumber == 0)
                 return firstNumber;
+            if (firstNumber < 0)
+                firstNumber = Math.Abs(firstNumber);
+            if (secondNumber < 0)
+                secondNumber = Math.Abs(secondNumber);
+            bool firstNumberIsEven = (firstNumber & 1u) == 0;
+            bool seondNumberIsEven = (secondNumber & 1u) == 0;
+            if (firstNumberIsEven && seondNumberIsEven)
+                return GetBinaryGCDOfTwoNumbers(firstNumber >> 1, secondNumber >> 1) << 1;
+            else if (firstNumberIsEven && !seondNumberIsEven)
+                return GetBinaryGCDOfTwoNumbers(firstNumber >> 1, secondNumber);
+            else if (seondNumberIsEven)
+                return GetBinaryGCDOfTwoNumbers(firstNumber, secondNumber >> 1);
+            else if (firstNumber > secondNumber)
+                return GetBinaryGCDOfTwoNumbers((firstNumber - secondNumber) >> 1, secondNumber);
             else
-            {
-                for (shift = 0; ((firstNumber | secondNumber) & 1) == 0; ++shift)
-                {
-                    firstNumber >>= 1;
-                    secondNumber >>= 1;
-                }
-                while ((firstNumber & 1) == 0)
-                    firstNumber >>= 1;
-                do
-                {
-                    while ((secondNumber & 1) == 0) 
-                        secondNumber >>= 1;
-                    if (firstNumber > secondNumber)
-                    {
-                        int temp = firstNumber;
-                        secondNumber = firstNumber;
-                        firstNumber = temp;
-                    }
-                    secondNumber = secondNumber - firstNumber; 
-                } while (secondNumber != 0);
-                return firstNumber << shift;
-            }           
+                return GetBinaryGCDOfTwoNumbers(firstNumber, (secondNumber - firstNumber) >> 1);
+
         }
 
     }
---
+++ EPAM.BSU.01.2016.Bytskevich.02/CalculatingGCDTests/GCDCalculatorClassTests.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+using CalculatingGCD;
+
+namespace CalculatingGCDTests
+{
+    [TestClass]
+    public class GCDCalculatorClassTests
+    {
+        #region Tests for Euclidean algorithm methods
+        [TestMethod]
+        public void GetGCD_TwoPositiveNumbers_NotNullGCD()
+        {
+            int arrangeGCD = 21;
+            string elapsedTime; 
+            int actGCD = GCDCalculator.GetGCD(252, 105, out elapsedTime);
+            Debug.WriteLine("GCD(252, 105) = {0}, algorithm took {1}", actGCD, elapsedTime);
+            Assert.AreEqual(arrangeGCD, actGCD);
+        }
+
+        [TestMethod]
+        public void GetGCD_TwoSameNegativeandPositiveNumbers_TheSamePositiveNumber()
+        {
+            int arrangeGCD = 12345;
+            string elapsedTime;
+            int actGCD = GCDCalculator.GetGCD(-12345, 12345, out elapsedTime);
+            Debug.WriteLine("GCD(12345, -12345) = {0}, algorithm took {1}", actGCD, elapsedTime);
+            Assert.AreEqual(arrangeGCD, actGCD);
+        }
+
+        [TestMethod]
+        public void GetGCD_OneOfTwoNumbersIsNull_GCDIsNull()
+        {
+            int arrangeGCD = 252;
+            string elapsedTime;
+            int actGCD = GCDCalculator.GetGCD(252, 0, out elapsedTime);
+            Debug.WriteLine("GCD(252, 0) = {0}, algorithm took {1}", actGCD, elapsedTime);
+            Assert.AreEqual(arrangeGCD, actGCD);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetGCD_BothNumbersAreNull_ArgumentException()
+        {
+            string elapsedTime;
+            int actGCD = GCDCalculator.GetGCD(0, 0, out elapsedTime);
+        }
+
+        [TestMethod]
+        public void GetGCD_OneOfTwoNumbersIsNegative_NotNullPositiveGCD()
+        {
+            int arrangeGCD = 21;
+            string elapsedTime;
+            int actGCD = GCDCalculator.GetGCD(252, -105, out elapsedTime);
+            Debug.WriteLine("GCD(252, -105) = {0}, algorithm took {1}", actGCD, elapsedTime);
+            Assert.AreEqual(arrangeGCD, actGCD);
+        }
+
+        [TestMethod]
+        public void GetGCD_ThreePositiveNumbers_NotNullGCD()
+        {
+            int arrangeGCD = 256;
+            string elapsedTime;
+            int actGCD = GCDCalculator.GetGCD(1024, 512, 256, out elapsedTime);
+            Debug.WriteLine("GCD(1024, 512, 256) = {0}, algorithm took {1}", actGCD, elapsedTime);
+            Assert.AreEqual(arrangeGCD, actGCD);
+        }
+
+        [TestMethod]
+        public void GetGCD_TwoOfThreeNumbersAreNegative_NotNullGCD()
+        {
+            int arrangeGCD = 256;
+            string elapsedTime;
+            int actGCD = GCDCalculator.GetGCD(1024, -512, -256, out elapsedTime);
+            Debug.WriteLine("GCD(1024, -512, -256) = {0}, algorithm took {1}", actGCD, elapsedTime);
+            Assert.AreEqual(arrangeGCD, actGCD);
+        }
+
+        [TestMethod]
+        public void GetGCD_TwoOfThreeNumbersAreNull_NotNullGCD()
+        {
+            int arrangeGCD = 256;
+            string elapsedTime;
+            int actGCD = GCDCalculator.GetGCD(0, 0, -256, out elapsedTime);
+            Debug.WriteLine("GCD(0, 0, -256) = {0}, algorithm took {1}", actGCD, elapsedTime);
+            Assert.AreEqual(arrangeGCD, actGCD);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetGCD_ThreeOfThreeNumbersAreNull_ArgumentException()
+        {
+            string elapsedTime;
+            int actGCD = GCDCalculator.GetGCD(0, 0, 0, out elapsedTime);
+        }
+
+        [TestMethod]
+        public void GetGCD_SevenPositiveNumbers_NotNullGCD()
+        {
+            int arrangeGCD = 2;
+            string elapsedTime;
+            int actGCD = GCDCalculator.GetGCD(out elapsedTime, 2, 4, 8, 16, 32, 64, 128) ;
+            Debug.WriteLine("GCD(2, 4, 8, 16, 32, 64, 128) = {0}, algorithm took {1}", actGCD, elapsedTime);
+            Assert.AreEqual(arrangeGCD, actGCD);
+        }
+
+        [TestMethod]
+        public void GetGCD_FiveOfTenNumbersAreNegative_NotNullGCD()
+        {
+            int arrangeGCD = 2;
+            string elapsedTime;
+            int actGCD = GCDCalculator.GetGCD(out elapsedTime, 2, -4, 8, -16, 32, -64, 128, -256, 512, -1024);
+            Debug.WriteLine("GCD( 2, -4, 8, -16, 32, -64, 128, -256, 512, -1024) = {0}, algorithm took {1}", actGCD, elapsedTime);
+            Assert.AreEqual(arrangeGCD, actGCD);
+        }
+
+        [TestMethod]
+        public void GetGCD_TenSameNegativeNumbers_NotNullGCD()
+        {
+            int arrangeGCD = 13;
+            string elapsedTime;
+            int actGCD = GCDCalculator.GetGCD(out elapsedTime, -13, -13, -13, -13, -13, -13, -13, -13, -13, -13);
+            Debug.WriteLine("GCD(-13, -13, -13, -13, -13, -13, -13, -13, -13, -13) = {0}, algorithm took {1}", actGCD, elapsedTime);
+            Assert.AreEqual(arrangeGCD, actGCD);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetGCD_FiveNullNumbers_ArgumentException()
+        {
+            string elapsedTime;
+            int actGCD = GCDCalculator.GetGCD(out elapsedTime, 0, 0, 0, 0, 0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetGCD_NoNumbersGiven_ArgumentException()
+        {
+            string elapsedTime;
+            int actGCD = GCDCalculator.GetGCD(out elapsedTime);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetGCD_OnlyOneNumberGiven_ArgumentException()
+        {
+            string elapsedTime;
+            int actGCD = GCDCalculator.GetGCD(out elapsedTime, -5);
+        }
+#endregion
+
+        #region Tests for Stein's (binary) algorithm methods
+        [TestMethod]
+        public void GetBinaryGCD_TwoPositiveNumbers_NotNullGCD()
+        {
+            int arrangeGCD = 13;
+            string elapsedTime;
+            int actGCD = GCDCalculator.GetGCDWithBinaryAlgorithm(169, 1326, out elapsedTime);
+            Debug.WriteLine("binary GCD(169, 1326) = {0}, algorithm took {1}", actGCD, elapsedTime);
+            Assert.AreEqual(arrangeGCD, actGCD);
+        }
+
+        [TestMethod]
+        public void GetBinaryGCD_TwoSameNegativeandPositiveNumbers_TheSamePositiveNumber()
+        {
+            int arrangeGCD = 12345;
+            string elapsedTime;
+            int actGCD = GCDCalculator.GetGCDWithBinaryAlgorithm(-12345, 12345, out elapsedTime);
+            Debug.WriteLine("GCD(12345, -12345) = {0}, algorithm took {1}", actGCD, elapsedTime);
+            Assert.AreEqual(arrangeGCD, actGCD);
+        }
+
+        [TestMethod]
+        public void GetBinaryGCD_OneOfTwoNumbersIsNull_GCDIsNull()
+        {
+            int arrangeGCD = 252;
+            string elapsedTime;
+            int actGCD = GCDCalculator.GetGCDWithBinaryAlgorithm(252, 0, out elapsedTime);
+            Debug.WriteLine("binary GCD(252, 0) = {0}, algorithm took {1}", actGCD, elapsedTime);
+            Assert.AreEqual(arrangeGCD, actGCD);
+        }
+        
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetBinaryGCD_BothNumbersAreNull_ArgumentException()
+        {
+            string elapsedTime;
+            int actGCD = GCDCalculator.GetGCDWithBinaryAlgorithm(0, 0, out elapsedTime);
+        }
+        
+        [TestMethod]
+        public void GetBinaryGCD_OneOfTwoNumbersIsNegative_NotNullPositiveGCD()
+        {
+            int arrangeGCD = 202;
+            string elapsedTime;
+            int actGCD = GCDCalculator.GetGCDWithBinaryAlgorithm(116150, -232704, out elapsedTime);
+            Debug.WriteLine("binary GCD(116150, -232704) = {0}, algorithm took {1}", actGCD, elapsedTime);
+            Assert.AreEqual(arrangeGCD, actGCD);
+        }
+        
+        [TestMethod]
+        public void GetBinaryGCD_ThreePositiveNumbers_NotNullGCD()
+        {
+            int arrangeGCD = 2560;
+            string elapsedTime;
+            int actGCD = GCDCalculator.GetGCDWithBinaryAlgorithm(10240, 5120, 2560, out elapsedTime);
+            Debug.WriteLine("binary GCD(10240, 5120, 2560) = {0}, algorithm took {1}", actGCD, elapsedTime);
+            Assert.AreEqual(arrangeGCD, actGCD);
+        }
+
+        [TestMethod]
+        public void GetBinaryGCD_TwoOfThreeNumbersAreNegative_NotNullGCD()
+        {
+            int arrangeGCD = 256;
+            string elapsedTime;
+            int actGCD = GCDCalculator.GetGCDWithBinaryAlgorithm(1024, -512, -256, out elapsedTime);
+            Debug.WriteLine("binary GCD(1024, -512, -256) = {0}, algorithm took {1}", actGCD, elapsedTime);
+            Assert.AreEqual(arrangeGCD, actGCD);
+        }
+
+        [TestMethod]
+        public void GetBinaryGCD_TwoOfThreeNumbersAreNull_NotNullGCD()
+        {
+            int arrangeGCD = 256;
+            string elapsedTime;
+            int actGCD = GCDCalculator.GetGCDWithBinaryAlgorithm(0, 0, -256, out elapsedTime);
+            Debug.WriteLine("binary GCD(0, 0, -256) = {0}, algorithm took {1}", actGCD, elapsedTime);
+            Assert.AreEqual(arrangeGCD, actGCD);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetBinaryGCD_ThreeOfThreeNumbersAreNull_ArgumentException()
+        {
+            string elapsedTime;
+            int actGCD = GCDCalculator.GetGCDWithBinaryAlgorithm(0, 0, 0, out elapsedTime);
+        }
+
+        [TestMethod]
+        public void GetBinaryGCD_SevenPositiveNumbers_NotNullGCD()
+        {
+            int arrangeGCD = 2;
+            string elapsedTime;
+            int actGCD = GCDCalculator.GetGCDWithBinaryAlgorithm(out elapsedTime, 2, 4, 8, 16, 32, 64, 128);
+            Debug.WriteLine("binary GCD(2, 4, 8, 16, 32, 64, 128) = {0}, algorithm took {1}", actGCD, elapsedTime);
+            Assert.AreEqual(arrangeGCD, actGCD);
+        }
+
+        [TestMethod]
+        public void GetBinaryGCD_FiveOfTenNumbersAreNegative_NotNullGCD()
+        {
+            int arrangeGCD = 2;
+            string elapsedTime;
+            int actGCD = GCDCalculator.GetGCDWithBinaryAlgorithm(out elapsedTime, 2, -4, 8, -16, 32, -64, 128, -256, 512, -1024);
+            Debug.WriteLine("binary GCD( 2, -4, 8, -16, 32, -64, 128, -256, 512, -1024) = {0}, algorithm took {1}", actGCD, elapsedTime);
+            Assert.AreEqual(arrangeGCD, actGCD);
+        }
+
+        [TestMethod]
+        public void GetBinaryGCD_TenSameNegativeNumbers_NotNullGCD()
+        {
+            int arrangeGCD = 13;
+            string elapsedTime;
+            int actGCD = GCDCalculator.GetGCDWithBinaryAlgorithm(out elapsedTime, -13, -13, -13, -13, -13, -13, -13, -13, -13, -13);
+            Debug.WriteLine("binary GCD(-13, -13, -13, -13, -13, -13, -13, -13, -13, -13) = {0}, algorithm took {1}", actGCD, elapsedTime);
+            Assert.AreEqual(arrangeGCD, actGCD);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetBinaryGCD_FiveNullNumbers_ArgumentException()
+        {
+            string elapsedTime;
+            int actGCD = GCDCalculator.GetGCDWithBinaryAlgorithm(out elapsedTime, 0, 0, 0, 0, 0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetBinaryGCD_NoNumbersGiven_ArgumentException()
+        {
+            string elapsedTime;
+            int actGCD = GCDCalculator.GetGCDWithBinaryAlgorithm(out elapsedTime);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetBinaryGCD_OnlyOneNumberGiven_ArgumentException()
+        {
+            string elapsedTime;
+            int actGCD = GCDCalculator.GetGCDWithBinaryAlgorithm(out elapsedTime, -5);
+        }
+        #endregion
+    }
+}
